88231712e9b2f386aef69797ae00916184fcdafd
Lisa Koivu <lisamarie.koivu@gmail.com>
2022-11-22 21:50:35 -0500

    Adding sql script to display recovery status in a friendly manner.

--- lambda/handler.py
+++ lambda/handler.py
@@ -20,37 +20,43 @@ if root.handlers:
         root.removeHandler(handler)
 logging.basicConfig(format='%(asctime)s %(message)s',level=logging.INFO)
 
-print('Loading function')
-
 secret_arn = os.getenv("CREDENTIALS_SECRET_ARN")
 
-sts_client = boto3.client("sts")
-account_number = sts_client.get_caller_identity()["Account"]
+# sts_client = boto3.client("sts")
+account_number = boto3.client("sts").get_caller_identity()["Account"]
 bucket_name=f"{account_number}-prod-data-archive"
 
 def handler(event, context):
 
     message = event['Records'][0]
-    logging.debug(f"From SNS: {message}")
+    logging.info(f"From SNS: {message}")
     
     # Fetch and parse secret.
     result = json.loads(get_secret(in_source_db_secret_arn=secret_arn))
-      
-    connect_string = result["connect_string"]
-    username = result["username"]
-    server = result["host"]
-    password = result["password"]
-    # Port not needed, it's in the connect string
-    # port = result["port"]
     
-
-    file_arn = "arn:aws:s3:::"+ bucket_name + "/" +  message["s3"]["object"]["key"]
+    # Parse out the items we need from the secret  
+    connect_string = result["connect_string"]
+    server = result["host"]
+    username = result["username"]
+    password = result["password"]
+    
+    logging.debug(f"message is {message}")
+    
+    # Grab the file name and bucket name from the event and construct the arn.
+    file_name = message["s3"]["object"]["key"]
+    bucket_arn = message["s3"]["bucket"]["arn"]
+    file_arn = bucket_arn + "/" +  file_name
+    
+    # This is currently hard coded. Remove this when deploying. 
+    # This is the recovery target db.
     dbname = "zAdministration"
-    conn_dbname="zAdmin2"
+    
+    # The recovery process must connect to a db that is online. Hard code
+    # this to the master db. 
+    conn_dbname="master"
 
-    logging.debug(f"username is {username}  {dbname} {file_arn} at {connect_string}")
+    logging.info(f"Recovery target database is {dbname}. File is {file_arn}")
 
-  
     cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+conn_dbname+';ENCRYPT=yes;UID='+username+';PWD='+ password)
     
     # Without autocommit, the rds stored proc call to the restore proc will fail.
@@ -58,7 +64,7 @@ def handler(event, context):
     cursor = cnxn.cursor()  
      
     recovery_command=f"exec msdb.dbo.rds_restore_log @restore_db_name={dbname}, @s3_arn_to_restore_from=\"{file_arn}\", @with_norecovery=1" 
-    logging.debug(f"recovery command is {recovery_command}") 
+    logging.info(f"recovery command is {recovery_command}") 
     
     cursor.execute(recovery_command)
 
@@ -68,10 +74,11 @@ def handler(event, context):
         # print(row[0])
         row = cursor.fetchone()
         print(f"{row}")
+    cursor.close()
 
     # This pulls all messages from mssql, so we can close the cursor gracefully.
     # Not doing this and closing the cursor can abort the restore. 
-    while cursor.nextset():
+    # while cursor.nextset():
         # pass 
         # cursor.close()        
 
